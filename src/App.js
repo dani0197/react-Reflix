fd0aaeb574abca8ad207fcf63e845b41fc698797
unknown <danny.michaelov@gmail.com>
2018-10-24 14:12:30 +0300

    done landing page + links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Landing from './components/Landing'
 import './App.css';
 
 class App extends Component {
@@ -14,7 +15,12 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-
+        <span className="main-links">
+          <Link claasName="Link" to="/">Home</Link>
+          <Link claasName="Link" id="reflix-logo" to="/catalog">Reflix</Link>
+          <Link claasName="Link" to="/catalog">Catalog</Link>
+        </span>
+        <Route exact path="/" component={Landing} />
         </div>
       </Router>
     );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import {Link} from 'react-router-dom';
+import '../styles/landing.css';
 
 class Landing extends Component {
     constructor() {
@@ -7,6 +8,20 @@ class Landing extends Component {
         this.state = {
 
         }
+    }
+
+    render() {
+        return (
+            <div>
+                <span><h3 id="landing-header">Whos watching?</h3></span>
+                <div className="users-container">
+                    <Link  to="/catalog"><div className="user" id="Mona">Mona</div></Link>
+                    <Link  to="/catalog"><div className="user" id="Jasmyne">Jasmyne</div></Link>
+                    <Link  to="/catalog"><div className="user" id="Aura">Aura</div></Link>
+                    <Link to="/catalog"><div className="user" id="Tina">Tina</div></Link>
+                </div>
+            </div>
+        )
     }
 }
 
